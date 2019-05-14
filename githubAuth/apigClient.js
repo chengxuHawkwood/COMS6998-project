e27b277b809db9d71ec9ee7e5b698cd171783a66
Terry1004 <hym961004@gmail.com>
2019-05-14 12:47:39 +0800

    Complete cognito and  github authorization.

---
+++ githubAuth/apigClient.js
@@ -0,0 +1,375 @@
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+var apigClientFactory = {};
+apigClientFactory.newClient = function (config) {
+    var apigClient = { };
+    if(config === undefined) {
+        config = {
+            accessKey: '',
+            secretKey: '',
+            sessionToken: '',
+            region: '',
+            apiKey: undefined,
+            defaultContentType: 'application/json',
+            defaultAcceptType: 'application/json'
+        };
+    }
+    if(config.accessKey === undefined) {
+        config.accessKey = '';
+    }
+    if(config.secretKey === undefined) {
+        config.secretKey = '';
+    }
+    if(config.apiKey === undefined) {
+        config.apiKey = '';
+    }
+    if(config.sessionToken === undefined) {
+        config.sessionToken = '';
+    }
+    if(config.region === undefined) {
+        config.region = 'us-east-1';
+    }
+    //If defaultContentType is not defined then default to application/json
+    if(config.defaultContentType === undefined) {
+        config.defaultContentType = 'application/json';
+    }
+    //If defaultAcceptType is not defined then default to application/json
+    if(config.defaultAcceptType === undefined) {
+        config.defaultAcceptType = 'application/json';
+    }
+
+    
+    // extract endpoint and path from url
+    var invokeUrl = 'https://huobgy5p9b.execute-api.us-east-1.amazonaws.com/dev';
+    var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
+    var pathComponent = invokeUrl.substring(endpoint.length);
+
+    var sigV4ClientConfig = {
+        accessKey: config.accessKey,
+        secretKey: config.secretKey,
+        sessionToken: config.sessionToken,
+        serviceName: 'execute-api',
+        region: config.region,
+        endpoint: endpoint,
+        defaultContentType: config.defaultContentType,
+        defaultAcceptType: config.defaultAcceptType
+    };
+
+    var authType = 'NONE';
+    if (sigV4ClientConfig.accessKey !== undefined && sigV4ClientConfig.accessKey !== '' && sigV4ClientConfig.secretKey !== undefined && sigV4ClientConfig.secretKey !== '') {
+        authType = 'AWS_IAM';
+    }
+
+    var simpleHttpClientConfig = {
+        endpoint: endpoint,
+        defaultContentType: config.defaultContentType,
+        defaultAcceptType: config.defaultAcceptType
+    };
+
+    var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(simpleHttpClientConfig, sigV4ClientConfig);
+    
+    
+    
+    apigClient.courseGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var courseGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/course').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(courseGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.coursePut = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var coursePutRequest = {
+            verb: 'put'.toUpperCase(),
+            path: pathComponent + uritemplate('/course').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(coursePutRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.coursePost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var coursePostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/course').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(coursePostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.courseDelete = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var courseDeleteRequest = {
+            verb: 'delete'.toUpperCase(),
+            path: pathComponent + uritemplate('/course').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(courseDeleteRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.courseOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var courseOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/course').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(courseOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.projectGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var projectGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/project').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(projectGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.projectPut = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var projectPutRequest = {
+            verb: 'put'.toUpperCase(),
+            path: pathComponent + uritemplate('/project').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(projectPutRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.projectPost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var projectPostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/project').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(projectPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.projectDelete = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var projectDeleteRequest = {
+            verb: 'delete'.toUpperCase(),
+            path: pathComponent + uritemplate('/project').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(projectDeleteRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.projectOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var projectOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/project').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(projectOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.projectGithubStatePut = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var projectGithubStatePutRequest = {
+            verb: 'put'.toUpperCase(),
+            path: pathComponent + uritemplate('/project/github-state').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(projectGithubStatePutRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.projectGithubStateOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var projectGithubStateOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/project/github-state').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(projectGithubStateOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.userGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var userGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/user').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(userGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.userPost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var userPostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/user').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(userPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.userDelete = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var userDeleteRequest = {
+            verb: 'delete'.toUpperCase(),
+            path: pathComponent + uritemplate('/user').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(userDeleteRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.userOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var userOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/user').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(userOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+
+    return apigClient;
+};
---
+++ githubAuth/manageAuth.js
@@ -0,0 +1,100 @@
+const promisify = func => {
+    return (...args) =>
+        new Promise((resolve, reject) => {
+            func(...args, (err, result) =>
+                err ? reject(err) : resolve(result)
+            );
+        });
+};
+
+// return a promise object resolved to current username
+const getUsername = cognitoUser => {
+    return promisify(cognitoUser.getUserAttributes.bind(cognitoUser))().then(
+        result => {
+            const username = result[2].getValue();
+            return Promise.resolve(username);
+        }
+    );
+};
+
+// fetch the list of users (empty or with 1 element) given username
+const fetchUserList = (apigClient, username) => {
+    const additionalParams = {
+        queryParams: {
+            usernmae: username
+        }
+    };
+    return apigClient
+        .userGet({}, {}, additionalParams)
+        .then(result => result.data);
+};
+
+const authorize = () => {
+    const loginUrl = new URL('https://github.com/login/oauth/authorize');
+    loginUrl.searchParams.set('client_id', 'c3e19ce6d629ea34b11e');
+    window.location.href = loginUrl;
+};
+
+const show = items => {
+    for (let item of items) {
+        item.style.display = 'initial';
+    }
+};
+
+const hide = items => {
+    for (let item of items) {
+        item.style.display = 'none';
+    }
+};
+
+const displayAuthorized = user => {
+    show(document.getElementsByClassName('authorized'));
+    document.getElementById('email_value').textContent = user.username;
+};
+
+const displayUnauthorized = () => {
+    show(document.getElementsByClassName('unauthorized'));
+};
+
+const displayAuthorizing = (apigClient, username, code) => {
+    const body = {
+        username: username,
+        code: code
+    };
+    apigClient.userPost({}, body, {}).then(() => {
+        window.location = 'profile.html';
+    });
+};
+
+const displayControl = userList => {
+    hide(document.getElementsByClassName('loading'));
+    if (userList.length > 0) {
+        // display user data since the user has already authorized github
+        const user = userList[0];
+        displayAuthorized(user);
+    } else {
+        // only display the button for github authorization
+        displayUnauthorized();
+    }
+};
+
+// render the page content
+const display = (cognitoUser, apigClient) => {
+    // the code paramter will be present if it is redirected from github authorization page
+    getUsername(cognitoUser)
+        .then(username => {
+            // check if the user has authorized github
+            const code = new URL(window.location.href).searchParams.get('code');
+            if (code) {
+                return displayAuthorizing(apigClient, username, code);
+            }
+            return Promise.resolve(username);
+        })
+        .then(username => {
+            return fetchUserList(apigClient, username);
+        })
+        .then(userList => {
+            // control the contents displayed to user
+            displayControl(userList);
+        });
+};
